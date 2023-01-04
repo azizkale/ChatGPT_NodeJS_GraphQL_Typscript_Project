1577c551f9137fc2e31beefb4f6b821c36d0fa23
azizkale <azizkale@hotmail.com>
2023-01-04 18:18:52 +0100

    the function that make example senteces added

--- src/schema/typeDefs/index.ts
+++ src/schema/typeDefs/index.ts
@@ -3,5 +3,6 @@ type Query {
     chat(question:String) : String
     correctGrammer(language: String, sentence: String): String
     translate(text:String, language:String):String
+    context(word:String , inputLanguage: String, outputLanguage:String): String
 }
 `;
